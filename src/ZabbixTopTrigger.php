bcfea96f9331f75e377a7bbcac87c658297a040b
Davi Gomes <davigomess@gmail.com>
2021-01-20 06:10:06 -0300

    Implementação do retorno em array das top triggers

--- src/ZabbixTopTrigger.php
+++ src/ZabbixTopTrigger.php
@@ -6,17 +6,50 @@ namespace ZabbixComponent;
 
 class ZabbixTopTrigger extends ZabbixComponent
 {
-
     public function getComponent($componentId = null)
     {
         $this->login();
 
-        return $this->httpClient->get('toptriggers.php', [
+        $request = $this->httpClient->get('toptriggers.php', [
             'query' => [
                 'from' => $this->startTime->format('Y-m-d H:i:s'),
                 'to' => $this->endTime->format('Y-m-d H:i:s'),
                 'profileIdx' => 'web.toptriggers.filter',
             ],
         ])->getBody()->getContents();
+
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        @$dom->loadHTML($request);
+        $rows = $dom->getElementsByTagName('tr');
+        $topTriggers = [];
+        foreach ($rows as $row) {
+            $topTrigger = [];
+            $index = 0;
+            foreach ($row->childNodes as $node) {
+                if ($node->nodeName === 'td') {
+                    switch ($index) {
+                        case 1:
+                            $key = 'trigger';
+                            break;
+                        case 2:
+                            $key = 'severity';
+                            break;
+                        case 3:
+                            $key = 'change_status';
+                            break;
+                        default:
+                            $key = 'host';
+                            break;
+                    }
+                    $topTrigger[$key] = $node->nodeValue;
+                    $index++;
+                }
+            }
+            if (empty($topTrigger) === false) {
+                $topTriggers[] = $topTrigger;
+            }
+        }
+
+        return $topTriggers;
     }
 }
